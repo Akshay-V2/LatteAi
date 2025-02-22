cedcd7a7e23e666bc137d6f895d2ab2d8be6b3a5
Akshay V V <akshcodes2008@gmail.com>
2025-02-22 21:14:53 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="main">
+      <div className="text-input"></div>
+    </div>
+  );
+}
+
+export default App;
