fce0b1b431e7450c2716864af142be6e29ed403e
Akshay V V <akshcodes2008@gmail.com>
2025-02-22 21:32:13 +0530

    added text input

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <div className="main">
-      <div className="text-input"></div>
+      <div className="text-input">
+        <input type="text" placeholder="Type your equation in words or describe it" className="input"></input>
+      </div>
     </div>
   );
 }
