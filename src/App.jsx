e99f7294ad97733286b38c3f3b80f8072912d7b1
Akshay V V <akshcodes2008@gmail.com>
2025-02-27 22:26:13 +0530

    added latex rendering support

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
 
 import Button from "./components/Button";
 
 function App() {
+  const [mode, setMode] = useState("launch");
+
+  const handleModeChange = (newMode) => {
+    setMode(newMode);
+  }
 
   return (
     <div className="main">
       <div className="text-input">
-        <input type="text" placeholder="Type your equation in words or describe it" className="input"></input>
+        {mode === "launch" ? <input type="text" placeholder="Type your equation in words or describe it" className="input"></input> : mode === "latex" ? <div className="latex"><Latex>$\frac{3}{4}$</Latex></div> : null}
       </div>
 
-      <div className="tools-right">
-        <Button text="Quit" keybind="Esc" highlight="false"></Button>
-        <Button text="Send" keybind="Ctrl+Enter" highlight="true"></Button>
+      <div className="tools">
+        {/* <select name="modelSelector" className="model-selector">
+          <option value="deepseek">Deepseek R1</option>
+          <option value="chatgpt-o1">ChatGPT o1</option>
+          <option value="claude">Claude</option>
+          <option value="llama">Llama</option>
+          <option value="gemini">Gemini</option>
+        </select> */}
+        
+        <div onClick={() => {handleModeChange("launch")}}><Button text="Quit" keybind="Esc" highlight="false"></Button></div>
+        <div className="spacer"></div>
+        <div onClick={() => {handleModeChange("latex")}}><Button text="Send" keybind="Ctrl+Enter" highlight="true"></Button></div>
       </div>
+
     </div>
   );
 }
